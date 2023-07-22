d0b92598ce6d31ece03602e17cd8c7e9cffffc86
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2023-07-22 14:12:51 +0800

    feat: add X-Application-Name header

--- application/src/main/java/com/example/application/Controller.java
+++ application/src/main/java/com/example/application/Controller.java
@@ -1,5 +1,8 @@
 package com.example.application;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +14,20 @@ import java.util.Map;
 @RestController()
 @RequestMapping("/api/v1")
 public class Controller {
+    @Value("${spring.application.name}")
+    private String name;
     @GetMapping("/ping")
     public String ping() {
         return "pong";
     }
 
     @PostMapping("/reflect")
-    public Map<String,Object> reflect(@RequestBody Map<String, Object> body) {
-        return body;
+    public ResponseEntity<Map<String,Object>> reflect(@RequestBody Map<String, Object> body) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("X-Application-Name", name);
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(body);
     }
 }
