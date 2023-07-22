6c49a40a844e35f47f15b8b8b260e0006199110d
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2023-07-22 17:34:38 +0800

    feat: implement rr logic

--- gateway/src/main/java/com/example/gateway/service/strategy/RoundRobinStrategy.java
+++ gateway/src/main/java/com/example/gateway/service/strategy/RoundRobinStrategy.java
@@ -3,8 +3,32 @@ package com.example.gateway.service.strategy;
 import java.util.List;
 
 public class RoundRobinStrategy implements Strategy {
+    int current;
+
+    public RoundRobinStrategy() {
+        this.current = 0;
+    }
+
     @Override
     public String elect(List<String> targets) {
-        return targets.get(0);
+        if(targets.isEmpty()) {
+            return "";
+        }
+        if(targets.size() == 1) {
+            return targets.get(0);
+        }
+
+        if(this.current >= targets.size()) {
+            this.current = 0;
+        }
+
+
+        return getHost(targets);
+    }
+
+    private synchronized String getHost(List<String> targets) {
+        String result = targets.get(this.current);
+        this.current++;
+        return result;
     }
 }
